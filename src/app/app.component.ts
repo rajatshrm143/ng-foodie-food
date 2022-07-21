a846df540a6c8552c9f61b1cf227bf64fc5dea30
rajatshrm143 <rajatshrm143@gmail.com>
2022-07-21 13:54:33 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component, VERSION } from '@angular/core';
-import { DoServiceService } from "./do-service.service"
+import { DoServiceService } from './do-service.service';
 // import { Book } from './book';
-import { Observable, Observer } from "rxjs";
-import { CookieService } from "ngx-cookie-service";
+import { Observable, Observer } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Employee Search';
@@ -17,15 +17,9 @@ export class AppComponent {
     setInterval(() => observer.next(new Date().toString()), 3000);
   });
 
-
   constructor(private serv: DoServiceService, private cookie: CookieService) {
     console.log('This is Main App module');
-
   }
 
-  ngOnInIt() {
-
-
-  }
-
+  ngOnInIt() {}
 }
